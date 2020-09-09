2786f9c97ba2f7a26bf073bac7ebec9ceda59fa0
Sui <admin@Mac-mini.local>
2020-09-08 23:31:40 -0400

    add function to detect when game is finished and the computer has guesed the number. and to navigate to a game over screen, implemented useEffect and stored an array of previously rendered values.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, Button, Alert } from "react-native";
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -18,9 +18,17 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
-
+    const [rounds, setRounds] = useState(0);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+        onGameOver(rounds);
+    };
+  }, [currentGuess, userChoice, onGameOver]); //removed props via destructuring so that they won't change whenever the parent changes.
 
   const nextGuessHandler = (direction) => {
     // validate the direction.
@@ -33,7 +41,7 @@ const GameScreen = (props) => {
       ]);
       return;
     }
-
+    // set min and max range for tne random number generator according to user hints.
     if (direction === 'lower') {
         currentHigh.current = currentGuess;
     } else {
@@ -42,6 +50,7 @@ const GameScreen = (props) => {
 
     const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
     setCurrentGuess(nextNumber);
+    setRounds(curRounds => curRounds +1);
   };
 
   return (
